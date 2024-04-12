66029615ed8be697b4254d9b53b99ff83f34a912
유형민 <hmy940424@naver.com>
2024-04-12 13:11:39 +0900

    feat: spring project

---
+++ BE/tripeer/src/test/java/j10d207/tripeer/TripeerApplicationTests.java
@@ -0,0 +1,13 @@
+package j10d207.tripeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
