11b8d9dd2b1cf602a9cda16bbba7353f9e093150
유형민 <hmy940424@naver.com>
2024-04-16 16:56:40 +0900

    feat : response custom

--- BE/tripeer/src/main/java/j10d207/tripeer/place/controller/PlaceController.java
+++ BE/tripeer/src/main/java/j10d207/tripeer/place/controller/PlaceController.java
@@ -1,22 +1,24 @@
 package j10d207.tripeer.place.controller;
 
-import j10d207.tripeer.exception.CustomException;
-import j10d207.tripeer.exception.ErrorCode;
+import j10d207.tripeer.place.service.CityServiceImpl;
+import j10d207.tripeer.response.Response;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-
 @RestController
 @RequestMapping("/place")
+@AllArgsConstructor
 public class PlaceController {
 
+    private final CityServiceImpl cityServicecImpl;
+
     @GetMapping("/city/{cityName}")
-    public String searchCity(@PathVariable("cityName") String cityName) {
-        
-        return cityName;
+    public Response<?> searchCity(@PathVariable("cityName") String cityName) {
+
+        return Response.of("ok", "test", "OJOJOJOJO");
     }
 
 }
--- BE/tripeer/src/main/java/j10d207/tripeer/place/service/CityServiceImpl.java
+++ BE/tripeer/src/main/java/j10d207/tripeer/place/service/CityServiceImpl.java
@@ -1,9 +1,12 @@
 package j10d207.tripeer.place.service;
 
+import j10d207.tripeer.place.db.dto.CityListDto;
 import j10d207.tripeer.place.db.repository.CityRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -13,4 +16,8 @@ public class CityServiceImpl implements CityService{
     private final CityRepository cityRepository;
 
 
+    @Override
+    public List<CityListDto> searchCity(String cityName) {
+        return null;
+    }
 }
