ed93cd5f928048df64478196ac86a5b80fb7082b
손동천 <kij8025@naver.com>
2024-04-16 17:12:08 +0900

    test

--- BE/tripeer/src/main/java/j10d207/tripeer/user/controller/UserController.java
+++ BE/tripeer/src/main/java/j10d207/tripeer/user/controller/UserController.java
@@ -1,7 +1,13 @@
 package j10d207.tripeer.user.controller;
 
+import j10d207.tripeer.exception.CustomException;
+import j10d207.tripeer.exception.ErrorCode;
+import j10d207.tripeer.response.Response;
+import j10d207.tripeer.user.db.dto.SocialInfoDTO;
+import j10d207.tripeer.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping("/test")
-    public String test(){
-        return "ok";
+    private UserService userService;
+
+    @GetMapping("/social/info")
+    public Response<SocialInfoDTO> socialInfo() {
+        try {
+            SocialInfoDTO socialInfoDTO = userService.getSocialInfo();
+            return Response.of(HttpStatus.OK, "OAuth 제공 정보", socialInfoDTO);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
 }
