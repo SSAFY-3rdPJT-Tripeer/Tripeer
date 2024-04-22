309a4a9c918843239a6cdb2c68ae16c5458ef992
lhgeer2617 <lhgeer2617@gmail.com>
2024-04-22 15:48:01 +0900

    [FE]  feat : 정보 입력 페이지 구현중

--- FE/tripeer/components/register/cancelBtn.jsx
+++ FE/tripeer/components/register/cancelBtn.jsx
@@ -1,24 +1,20 @@
 import styles from "./cancelBtn.module.css";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 
-<<<<<<< HEAD
-export default function CancelBtn({ pageNum, setPageNum }) {
-  const router = useRouter();
-  const cancelOnClick = () => {
-    setPageNum(pageNum - 1);
-=======
-export default function CancelBtn() {
-  const router = useRouter();
-  const cancelOnClick = () => {
-    router.back();
->>>>>>> origin/fe/feat/login
-  };
 
-  return (
-    <div
-      className={`${styles.center} ${styles.cancel}`}
-      onClick={cancelOnClick}>
-      취소
-    </div>
-  );
+export default function CancelBtn({pageNum, setPageNum}) {
+    const router = useRouter();
+    const cancelOnClick = () => {
+        setPageNum(pageNum - 1);
+
+
+    };
+
+    return (
+        <div
+            className={`${styles.center} ${styles.cancel}`}
+            onClick={cancelOnClick}>
+            취소
+        </div>
+    );
 }
