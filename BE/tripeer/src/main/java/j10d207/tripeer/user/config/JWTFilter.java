714780f1a03b7e3e03b787b2f335ddcdb280cafa
손동천 <kij8025@naver.com>
2024-04-24 15:11:45 +0900

    [BE] fix : 확인3

--- BE/tripeer/src/main/java/j10d207/tripeer/user/config/JWTFilter.java
+++ BE/tripeer/src/main/java/j10d207/tripeer/user/config/JWTFilter.java
@@ -58,7 +58,7 @@ public class JWTFilter extends OncePerRequestFilter {
 
         // 토큰이 access인지 확인 (발급시 페이로드에 명시)
         String category = jwtUtil.getCategory(accessToken);
-        if (!category.equals("Authorization")) {
+        if (!category.equals("access")) {
 
             //response body
             PrintWriter writer = response.getWriter();
