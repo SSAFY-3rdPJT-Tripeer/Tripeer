4acec4a968a40003b1fb686d4b529a00ba338ce0
lhgeer2617 <lhgeer2617@gmail.com>
2024-04-25 16:26:33 +0900

    [FE] feat : 여행지 페이지 구조 수정

--- FE/tripeer/components/place/placeItem.jsx
+++ FE/tripeer/components/place/placeItem.jsx
@@ -1,34 +1,22 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-import api from "@/utils/api";
 import styles from "./placeItem.module.css";
-// import mainSrc from "./asset/sample.png";
 import vectorSrc from "@/public/place/vector.png";
 
-export default function PlaceItem({ data, setList, isCity }) {
+export default function PlaceItem({ data, isCity }) {
+  const router = useRouter();
+
   const onClick = () => {
     // 클릭 시, 씨티인 경우
     if (data.townName === undefined) {
-      // 해당 씨티의 타운 전체 리스트를 GET 해서 리스트를 갱신
-      getTownData();
+      // 해당 씨티의 타운을 보여주는 페이지로 이동
+      router.push(`/place/${data.cityId}`);
     }
     // 클릭 시, 타운인 경우
     else {
       // 해당 타운의 디테일 페이지로 이동
-    }
-  };
-
-  const getTownData = async () => {
-    try {
-      const res = await api.get("/place/town", {
-        params: { cityId: data.cityId, townName: -1 },
-      });
-      setList([]);
-      const tmp = [...res.data.data];
-      setList(tmp);
-      // console.log("타운 ", res.data.data);
-    } catch (e) {
-      console.log("타운 정보 요청 에러 : ", e);
+      router.push("/place/detail");
     }
   };
 
