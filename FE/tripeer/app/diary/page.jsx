75abb4a98b25777505a4c891545148ee772a1cb3
hyunjihye <hhh259@naver.com>
2024-04-25 14:47:04 +0900

    [FE] feat :diary목록 페이지 생성

--- FE/tripeer/app/diary/page.jsx
+++ FE/tripeer/app/diary/page.jsx
@@ -1,5 +1,18 @@
+import styles from "./page.module.css";
+import DiaryCard from "@/components/diary/diaryCard";
+
 const Diary = () => {
-  return <div>diary</div>;
+  return (
+    <main className={styles.container}>
+      <header className={styles.banner}>
+        <p className={styles.title}>지나온 당신의</p>
+        <p className={styles.title}>여행을 기억합니다.</p>
+        <p className={styles.subTitle}>지나온 여행을 추억해 보아요.</p>
+      </header>
+
+      <DiaryCard></DiaryCard>
+    </main>
+  );
 };
 
 export default Diary;
---
+++ FE/tripeer/components/diary/diaryCard.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+/* eslint-disable */
+
+// 외부 모듈
+import { useEffect, useRef, useState } from "react";
+
+// 내부 모듈
+import { card } from "./diaryDummy";
+import styles from "./diaryCard.module.css";
+import Image from "next/image";
+
+const DiaryCard = () => {
+  const [dummy, setDummy] = useState([]);
+  useEffect(() => {
+    setDummy(card);
+  }, []);
+  console.log(dummy);
+  return (
+    <main className={styles.container}>
+      <div className={styles.cardBox}>
+        {dummy.map((item, idx) => {
+          return (
+            <div className={styles.card} key={idx}>
+              <div
+                className={styles.cardImgBox}
+                style={{ position: "relative" }}>
+                <Image
+                  src={item.Img}
+                  fill
+                  alt="im"
+                  className={styles.cardImg}
+                />
+              </div>
+              <div className={styles.contentBox}>
+                <p className={styles.title}>{item.title}</p>
+                <p className={styles.placeList}>
+                  {item.townList.map((place, id) => {
+                    return (
+                      <p key={id} className={styles.placeText}>
+                        {id === item.townList.length - 1 ? (
+                          <span>{place}</span>
+                        ) : (
+                          <span>{`${place}, `}</span>
+                        )}
+                      </p>
+                    );
+                  })}
+                </p>
+                <div className={styles.bottomBox}>
+                  <p className={styles.dateText}>
+                    {item.startDay} - {item.endDay}
+                  </p>
+                  <div className={styles.memberBox}>
+                    <div className={styles.memberIcon}></div>
+                    <div className={styles.memberText}>
+                      {item.member?.length}명
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default DiaryCard;
