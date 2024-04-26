a7b43fcc29815916b86ab9d10a78a4e9d92543c9
suehwanBoo <rndjdieo119@naver.com>
2024-04-26 09:44:54 +0900

    [FE] feat : 플랜 디테일 Nav 완성

--- FE/tripeer/app/plan/detail/[id]/page.jsx
+++ FE/tripeer/app/plan/detail/[id]/page.jsx
@@ -1,15 +1,30 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+import PlanNav from "@/components/nav/PlanNav";
+import styles from "./page.module.css";
+import PlanHome from "@/components/plan/detail/PlanHome";
+import PlanMap from "@/components/plan/detail/PlanMap";
+import PlanSchedule from "@/components/plan/detail/PlanSchedule";
 
 const PageDetail = (props) => {
+  const [current, setCurrent] = useState(0);
   useEffect(() => {
+    // 여기서 구독 처리( 해당 props에 planId 존재함)
     console.log(props);
   }, [props]);
+  const RENDER = useMemo(() => {
+    return [
+      <PlanHome key={"PlanHome"} />,
+      <PlanMap key={"PlanMap"} />,
+      <PlanSchedule key={"PlanSchedule"} />,
+    ];
+  }, []);
   return (
-    <div>
-      <h1>agdgadg</h1>
-    </div>
+    <main className={styles.container}>
+      <PlanNav current={current} setCurrent={setCurrent}></PlanNav>
+      {RENDER[current]}
+    </main>
   );
 };
 
---
+++ FE/tripeer/components/plan/detail/PlanHome.jsx
@@ -0,0 +1,9 @@
+const PlanHome = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default PlanHome;
