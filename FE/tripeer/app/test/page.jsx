062392b6238dd76ded104a342d7da3a74472bace
lhgeer2617 <lhgeer2617@gmail.com>
2024-04-27 20:09:25 +0900

    [FE] fix : test 페이지 주석처리

--- FE/tripeer/app/test/page.jsx
+++ FE/tripeer/app/test/page.jsx
@@ -1,27 +1,27 @@
-"use client";
-
-import Loading from "@/components/loading/loading";
-import { useEffect, useState } from "react";
-
-const Test = () => {
-  const [step, setStep] = useState(2);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setStep(1);
-    }, 1500);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
-
-  useEffect(() => {}, [step]);
-
-  return (
-    <div>
-      <Loading step={step}></Loading>
-    </div>
-  );
-};
-
-export default Test;
+// "use client";
+//
+// import Loading from "@/components/loading/loading";
+// import { useEffect, useState } from "react";
+//
+// const Test = () => {
+//   const [step, setStep] = useState(2);
+//
+//   useEffect(() => {
+//     const timeout = setTimeout(() => {
+//       setStep(1);
+//     }, 1500);
+//     return () => {
+//       clearTimeout(timeout);
+//     };
+//   }, []);
+//
+//   useEffect(() => {}, [step]);
+//
+//   return (
+//     <div>
+//       <Loading step={step}></Loading>
+//     </div>
+//   );
+// };
+//
+// export default Test;
