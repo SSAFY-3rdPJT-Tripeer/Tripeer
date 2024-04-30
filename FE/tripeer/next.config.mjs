4d80f43e7fc2cb267c48fe72668220e293ab85ca
suehwanBoo <rndjdieo119@naver.com>
2024-04-30 15:15:03 +0900

    [FE] fix : 이미지 에러 config

--- FE/tripeer/next.config.mjs
+++ FE/tripeer/next.config.mjs
@@ -1,47 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.visitkorea.or.kr",
-      },
-      {
-        protocol: "https",
-        hostname: "conlab.visitkorea.or.kr",
-      },
-      {
-        protocol: "https",
-        hostname: "search.pstatic.net",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn1.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn2.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "tripeerbucket.s3.ap-southeast-2.amazonaws.com",
-      },
-      {
-        protocol: "http",
-        hostname: "tong.visitkorea.or.kr",
-      },
+    domains: [
+      "cdn.visitkorea.or.kr",
+      "tong.visitkorea.or.kr",
+      "conlab.visitkorea.or.kr",
+      "lh3.googleusercontent.com",
+      "lh5.googleusercontent.com",
+      "encrypted-tbn2.gstatic.com",
+      "search.pstatic.net",
+      "encrypted-tbn0.gstatic.com",
+      "encrypted-tbn1.gstatic.com",
+      "tripeerbucket.s3.ap-southeast-2.amazonaws.com",
     ],
   },
-  reactStrictMode: false,
-
+  resactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
