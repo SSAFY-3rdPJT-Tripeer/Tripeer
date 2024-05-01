b61d2db09546c0e3fe4baa4e02549ffaecae38ef
lhgeer2617 <lhgeer2617@gmail.com>
2024-05-01 18:03:38 +0900

    [FE] feat : 슈퍼유저 추가 /플랜 스케줄 구현중

--- FE/tripeer/components/nav/superUserBtn.jsx
+++ FE/tripeer/components/nav/superUserBtn.jsx
@@ -1,16 +1,27 @@
+"use client";
+
 import axios from "axios";
 import cookies from "js-cookie";
 
-export default function SuperUserBtn() {
+import styles from "./superUserBtn.module.css";
+import { useRouter } from "next/navigation";
+
+export default function SuperUserBtn({ id }) {
+  const router = useRouter();
   const onClick = async () => {
     await axios
-      .get("https://k10d207.p.ssafy.io/api/user/test/getsuper/1")
+      .get(`https://k10d207.p.ssafy.io/api/user/test/getsuper/${id}`)
       .then((res) => {
         const accessToken = res.data.data.replace("Bearer ", "");
         // localStorage.setItem("accessToken", accessToken);
         cookies.set("Authorization", accessToken);
+        router.push("/");
       });
   };
 
-  return <button onClick={onClick}>딸깍</button>;
+  return (
+    <button className={styles.main} onClick={onClick}>
+      딸깍
+    </button>
+  );
 }
--- FE/tripeer/components/plan/detail/PlanSchedule.jsx
+++ FE/tripeer/components/plan/detail/PlanSchedule.jsx
@@ -1,64 +1,199 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
 import styles from "./PlanSchedule.module.css";
 import flagSrc from "@/public/plan/flag.png";
 import scheduleIcon from "@/public/plan/scheduleIcon.png";
+import updateIcon from "@/public/plan/update.png";
+
 // 더미 데이터
 import dummyUserList from "@/utils/dummyUserList";
 import dummyPlaceList from "@/utils/dummyItem";
 import ScheduleItem from "@/components/plan/detail/schedule/scheduleItem";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const PlanSchedule = () => {
   const [userList, setUserList] = useState([]);
-  const [placeList, setPlaceList] = useState([]);
+  // const [placeList, setPlaceList] = useState([]);
+  const [totalList, setTotalList] = useState([]);
+
+  // 아이템을 놓을때 실행
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+
+    // 이상한곳에 드롭되는경우
+    if (!destination) {
+      return;
+    }
+
+    // 가져온 영역
+    const sIdx = +source.droppableId;
+    // 옮긴 영역
+    const dIdx = +destination.droppableId;
+
+    // 같은 영역인 경우
+    if (sIdx === dIdx) {
+      // 전체 배열 가져오기
+      let tList = [...totalList];
+      // 원래 있던 곳의 배열 가져오기
+      let sList = [...tList[sIdx]];
+      // 전체 배열에서 수정할 배열 지우기
+      tList.splice(sIdx, 1);
+
+      // 옮길 데이터 임시 저장
+      const tmp = sList[source.index];
+
+      // 데이터가 원래 있던 곳에서 지우고
+      sList.splice(source.index, 1);
+      // 데이터가 이동한 곳에 넣기
+      sList.splice(destination.index, 0, tmp);
+
+      // 전체 배열에서 수정된 새 배열 원래 위치에 넣기
+      tList.splice(dIdx, 0, sList);
+
+      // 화면에 랜더링하느 전체 배열 갱신
+      setTotalList(tList);
+    }
+
+    // 다른 영역인 경우
+    // 다른 영역인 경우
+    else {
+      // 전체 배열 가져오기
+      let tList = [...totalList];
+      // 원래 있던 곳의 배열 가져오기
+      let sList = [...tList[sIdx]];
+      // 이동 한 곳의 배열 가져오기
+      let dList = [...tList[dIdx]];
+
+      // 전체 배열에서 수정할 배열 지우기
+      tList.splice(sIdx, 1);
+
+      // 옮길 데이터 임시 저장
+      const tmp = sList[source.index];
+
+      // 데이터가 원래 있던 곳에서 지우고
+      sList.splice(source.index, 1);
+      // 데이터가 이동한 곳에 넣기
+      dList.splice(destination.index, 0, tmp);
+
+      //전체 배열에서 원래 있던 곳의 배열 수정본 원래 위치에 넣기
+      tList.splice(sIdx, 0, sList);
+      // 전체 배열에서 이동한 곳의 배열 수정본 원래 위치에 넣기
+      tList.splice(dIdx, 0, dList);
+
+      // 화면에 랜더링하느 전체 배열 갱신
+      setTotalList(tList);
+    }
+
+    // const tmp = placeList[source.index];
+    // let tmpList = [...placeList];
+    // tmpList.splice(source.index, 1);
+    // tmpList.splice(destination.index, 0, tmp);
+    // setPlaceList(tmpList);
+  };
 
   useEffect(() => {
     setUserList(dummyUserList);
-    setPlaceList(dummyPlaceList);
+    // setPlaceList(dummyPlaceList);
+    setTotalList([dummyPlaceList, [], [], [], []]);
   }, []);
 
   return (
     // 화면 전체
     <div className={styles.container}>
-      {/*  왼쪽 우리의 여행지 목록 전체 박스  */}
-      <main className={styles.main}>
-        {/*  우리의 여행지 목록 타이틀 박스  */}
-        <section className={styles.titleBox}>
-          <Image src={flagSrc} alt={""} width={23} height={26} />
-          <p>우리의 여행지 목록</p>
-        </section>
-        <div className={styles.line} />
-        {/*  유저 프사 박스  */}
-        <section className={styles.userBox}>
-          {userList.map((el, idx) => {
-            return <img key={idx} src={el.img} alt={""} />;
-          })}
-        </section>
-        <div className={styles.line} />
-        {/*  왼쪽 바디 */}
-        {/*  목록 리스트  */}
-        <section className={`${styles.list} ${styles.scroll}`}>
-          {placeList.map((el, idx) => {
-            return <ScheduleItem key={idx} data={el} />;
-          })}
-        </section>
-      </main>
-      {/*  오른쪽 플랜 일정 짜는 바디  */}
-      {/*  오른쪽 일정 박스  */}
-      <main className={styles.mainRight}>
-        {/*  오른쪽 헤더  */}
-        <header className={styles.rightHeader}>
-          {/*  헤더의 왼쪽 부분  */}
-          <section className={styles.leftSection}>
-            <Image src={scheduleIcon} alt={""} width={30} height={30} />
-            <p>여행일정</p>
-            <p>24.05.05(월) - 24.05.08(목)</p>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {/*  왼쪽 우리의 여행지 목록 전체 박스  */}
+        <main className={styles.main}>
+          {/*  우리의 여행지 목록 타이틀 박스  */}
+          <section className={styles.titleBox}>
+            <Image src={flagSrc} alt={""} width={15} height={18} />
+            <p>우리의 여행지 목록</p>
           </section>
-          {/*  헤더의 오른쪽 부분  */}
-        </header>
-        <div className={styles.line} />
-      </main>
+          <div className={styles.line} />
+          {/*  유저 프사 박스  */}
+          <section className={styles.userBox}>
+            {userList.map((el, idx) => {
+              return <img key={idx} src={el.img} alt={""} />;
+            })}
+          </section>
+          <div className={styles.line} />
+          {/*-----------------------------------------------------------------------------------*/}
+          {/*  왼쪽 바디 */}
+          {/*  목록 리스트  */}
+          {/*dnd*/}
+          {/*dnd 영역*/}
+          {/*droppableId는 영역 아이디*/}
+          <Droppable droppableId={"0"}>
+            {(provided) => (
+              <section
+                className={`${styles.list} ${styles.scroll}`}
+                ref={provided.innerRef}
+                {...provided.droppableProps}>
+                {totalList[0]?.map((el, idx) => {
+                  return (
+                    //   잡을 수 있는 아이템들
+                    //   draggableId는 잡을 수 있는 아이템들의 아이디
+                    //   index 는 onDragEnd 에서 result 값에 있는 source destination 의 인덱스 값
+                    <Draggable
+                      key={idx}
+                      draggableId={`draggable-${idx}`}
+                      index={idx}>
+                      {(provided) => (
+                        //   ref 를 줘야하기 때문에 div 로 한번 감싸서 dnd 속성들을 div 에 넣는데
+                        <div
+                          key={idx}
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className={styles.itemBox}>
+                          <ScheduleItem data={el} />
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </section>
+            )}
+          </Droppable>
+        </main>
+        {/*  오른쪽 플랜 일정 짜는 바디  */}
+        {/*  오른쪽 일정 박스  */}
+        {/*-----------------------------------------------------------------------------------*/}
+        <main className={styles.mainRight}>
+          {/*  오른쪽 헤더  */}
+          <header className={styles.rightHeader}>
+            {/*  헤더의 왼쪽 부분  */}
+            <section className={styles.leftSection}>
+              <Image src={scheduleIcon} alt={""} width={18} height={18} />
+              <p>여행일정</p>
+              <p>24.05.05(월) - 24.05.08(목)</p>
+              <Image
+                className={styles.update}
+                src={updateIcon}
+                alt={""}
+                width={18}
+                height={18}
+              />
+            </section>
+            {/*  헤더의 오른쪽 부분  */}
+          </header>
+          <div className={styles.line} />
+          {/*  ---------------------------------------------------------------------------------*/}
+          {/*  오른쪽 일정 관리 박스  */}
+          <div className={styles.scheduleContainer}>
+            <section className={styles.scheduleSection}>
+              <header>
+                <p className={styles.scP1}>1일차</p>
+                <p className={styles.scP2}>24.05.05(월)</p>
+              </header>
+            </section>
+          </div>
+          {/*  ---------------------------------------------------------------------------------*/}
+        </main>
+      </DragDropContext>
     </div>
   );
 };
