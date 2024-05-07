d512755d6ce33919d2b2ddcc4fc3b2cfb9463df0
hyunjihye <hhh259@naver.com>
2024-05-07 21:19:44 +0900

    [FE] feat :diarycard 추가해서 다시 빌드시도

--- FE/tripeer/app/diary/page.jsx
+++ FE/tripeer/app/diary/page.jsx
@@ -1,12 +1,12 @@
 import styles from "./page.module.css";
 import DiaryBanner from "@/components/diary/DiaryBanner";
-// import DiaryCard from "@/components/diary/DiaryCard";
+import DiaryCard from "@/components/diary/DiaryCard";
 
 const Diary = () => {
   return (
     <main className={styles.container}>
       <DiaryBanner></DiaryBanner>
-      {/* <DiaryCard></DiaryCard> */}
+      <DiaryCard></DiaryCard>
     </main>
   );
 };
