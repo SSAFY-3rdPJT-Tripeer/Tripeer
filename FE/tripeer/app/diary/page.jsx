a2f6b75a1f58e2af2c9e89a0dbf9c38ab92751d1
hyunjihye <hhh259@naver.com>
2024-05-07 21:24:03 +0900

    [FE] test : diartCard > diaryCardList로 컴포넌트 변경 후 빌드 테스트

--- FE/tripeer/app/diary/page.jsx
+++ FE/tripeer/app/diary/page.jsx
@@ -1,12 +1,12 @@
 import styles from "./page.module.css";
 import DiaryBanner from "@/components/diary/DiaryBanner";
-import DiaryCard from "@/components/diary/DiaryCard";
+import DiaryCardList from "@/components/diary/DiaryCardList.jsx";
 
 const Diary = () => {
   return (
     <main className={styles.container}>
       <DiaryBanner></DiaryBanner>
-      <DiaryCard></DiaryCard>
+      <DiaryCardList></DiaryCardList>
     </main>
   );
 };
