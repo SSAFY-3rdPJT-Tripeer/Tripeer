45320c0ba78fe7f5616aad54306ee5eadbf09b36
유형민 <hmy940424@naver.com>
2024-05-08 16:51:35 +0900

    [BE] refactor : 이메일 발송

--- BE/tripeer/src/main/java/j10d207/tripeer/email/controller/EmailController.java
+++ BE/tripeer/src/main/java/j10d207/tripeer/email/controller/EmailController.java
@@ -3,9 +3,11 @@ package j10d207.tripeer.email.controller;
 
 import j10d207.tripeer.email.db.dto.EmailDTO;
 import j10d207.tripeer.email.service.EmailService;
+import j10d207.tripeer.response.Response;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,7 +19,7 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("")
-    public boolean test(@RequestBody EmailDTO emailDTO) {
-        return emailService.sendEmail(emailDTO);
+    public Response<?> test(@RequestBody EmailDTO emailDTO) {
+        return Response.of(HttpStatus.OK, "이메일 전송 완료", emailService.sendEmail(emailDTO));
     }
 }
