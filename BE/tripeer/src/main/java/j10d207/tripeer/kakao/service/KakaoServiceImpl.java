5b93f8a91f938dcc4ba836cb9c78b7e1b84a0991
유형민 <hmy940424@naver.com>
2024-05-12 18:05:39 +0900

    [BE] refactor : 머지 병합 오류 해결

--- BE/tripeer/src/main/java/j10d207/tripeer/kakao/service/KakaoServiceImpl.java
+++ BE/tripeer/src/main/java/j10d207/tripeer/kakao/service/KakaoServiceImpl.java
@@ -159,22 +159,6 @@ public class KakaoServiceImpl implements KakaoService{
         return totalTime / 60;
     }
 
-    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        // Haversine 공식을 사용하여 거리 계산
-        double earthRadius = 6371; // 지구 반지름 (단위: 킬로미터)
-
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lon2 - lon1);
-
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
-                        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        return earthRadius * c;
-    }
-
     private JsonElement getBestTime(JsonArray itineraries) {
         int minTime = Integer.MAX_VALUE;
         JsonElement bestJson = new JsonObject();
