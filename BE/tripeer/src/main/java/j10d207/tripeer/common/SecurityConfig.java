d5712c3139a492ce392c620b73f922da1947a141
손동천 <kij8025@naver.com>
2024-05-14 17:59:52 +0900

    [BE] fix : 비회원 권한 추가 닉네임 중복체크 mapping

--- BE/tripeer/src/main/java/j10d207/tripeer/common/SecurityConfig.java
+++ BE/tripeer/src/main/java/j10d207/tripeer/common/SecurityConfig.java
@@ -93,7 +93,8 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(HttpMethod.GET, "/user/error", "/swagger-ui/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/user/signup", "/user/name/duplicatecheck/*").hasAnyRole("VALIDATE")
+                        .requestMatchers(HttpMethod.GET, "/user/name/duplicatecheck/*").hasAnyRole("VALIDATE")
+                        .requestMatchers(HttpMethod.POST, "/user/signup").hasAnyRole("VALIDATE")
                         //배포시 test 삭제 필요
                         .requestMatchers(HttpMethod.GET, "/user/test/**", "/user/social/info", "/user/name/duplicatecheck/*", "/weather", "/history/*").hasAnyRole("NONE", "USER", "ADMIN")
                         .requestMatchers(HttpMethod.POST, "/user/reissue").hasAnyRole("NONE", "USER", "ADMIN")
